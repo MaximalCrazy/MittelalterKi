a4787dcfa55dad21a220d3511195eed2532a88ae
Timo <timofalkenhain@gmail.com>
2020-04-28 20:08:55 +0200

    Vorlage Model

---
+++ MittelalterKi/Data/Models/Bonus.cs
@@ -0,0 +1,10 @@
+﻿namespace MittelalterKi.Data.Models
+{
+    /// <summary>
+    /// Beinhaltet alle positiven, wie auch negativen Bonis einer Person
+    /// </summary>
+    public class Bonus
+    {
+        //Beispiel: Rüstungswert Vorteile
+    }
+}

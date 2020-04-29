1408c459a3f2c331e6e8934e74fb7bbd1807a660
Thomas P <43186116+T-mp@users.noreply.github.com>
2020-04-29 15:36:21 +0200

    PoC StateMachineKi

---
+++ MittelalterKi/Data/StateMachine/BauerPoC.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using MittelalterKi.Data.StateMachine.Bedürfnise;
+using MittelalterKi.Data.StateMachine.Fähigkeiten;
+using MittelalterKi.Data.StateMachine.Handlungen;
+
+namespace MittelalterKi.Data.StateMachine
+{
+    public class BauerPoC : Individuum
+    {
+        private static readonly System.Random rnd = new System.Random();
+
+        protected readonly IFähigkeitenQuelle fähigkeitenQuelle;
+
+        public BauerPoC(ILogger<BauerPoC> logger, IFähigkeitenQuelle fähigkeitenQuelle = null) : base(logger)
+        {
+            Name = $"{nameof(BauerPoC)} Nr.: {Id}";
+            this.fähigkeitenQuelle = fähigkeitenQuelle;
+            bedürfnise.Add(new Bedürfnis("Narung", rnd.Next(10,100)));
+            bedürfnise.Add(new Bedürfnis("Wasser", rnd.Next(20, 80)));
+
+            state = new Warte(this, logger);
+        }
+    }
+}
---
+++ MittelalterKi/Data/StateMachine/Individuum.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using MittelalterKi.Data.StateMachine.Bedürfnise;
+using MittelalterKi.Data.StateMachine.Handlungen;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MittelalterKi.Data.StateMachine
+{
+    public abstract class Individuum
+    {
+        private static int sID = 1;
+        public int Id { get; } = sID++;
+
+        public string Name { get; set; }
+
+        public List<string> Zustände { get; } = new List<string>();
+
+        public IReadOnlyList<IBedürfnis> Bedürfnise => (IReadOnlyList<IBedürfnis>)bedürfnise;
+        protected IList<IBedürfnis> bedürfnise = new List<IBedürfnis>();
+
+        protected IHandlung state;
+        protected readonly ILogger<Individuum> logger;
+
+
+        public Individuum(ILogger<Individuum> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task BerechneNächstenZustand(decimal zeitEinheiten = 1)
+        {
+            logger.LogTrace($"BerechneNächstenZustand({zeitEinheiten})");
+            state = await state.BerechneNächste(zeitEinheiten);
+        }
+    }
+}
